2111414156b356ec61de476994a5b9b804f34370
Okaeri <laugalung@gmail.com>
2020-10-04 16:18:05 +0800

    Prob0104Solution 二叉树的最大深度 迭代解法

---
+++ src/leetcode/Prob0104MaximumDepthOfBinaryTree/Prob0104Solution.java
@@ -0,0 +1,32 @@
+package leetcode.Prob0104MaximumDepthOfBinaryTree;
+
+/**
+ * 迭代解法
+ *
+ */
+public class Prob0104Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        else return maxDepth(root.left, root.right) + 1;
+    }
+    public int maxDepth(TreeNode p, TreeNode q){
+        if (p == null && q == null) return 0;
+        else if (p == null) return maxDepth(q);
+        else if (q == null) return maxDepth(p);
+        else  return Math.max(maxDepth(p.left, p.right) + 1, maxDepth(q.left, q.right) + 1);
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
