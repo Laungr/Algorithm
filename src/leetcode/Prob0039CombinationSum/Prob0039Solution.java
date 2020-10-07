092f668479c004609604f8656b9886c3c8a48aad
Okaeri <laugalung@gmail.com>
2020-10-07 22:08:38 +0800

    Prob0039Solution2 增加了一种高效点的写法，也是回溯

--- src/leetcode/Prob0039CombinationSum/Prob0039Solution.java
+++ src/leetcode/Prob0039CombinationSum/Prob0039Solution.java
@@ -14,7 +14,7 @@ public class Prob0039Solution {
 
     }
 
-    private void combinationSum(int[] candidates, int target, Set<List<Integer>> result, ArrayList<Integer> subList) {
+    public void combinationSum(int[] candidates, int target, Set<List<Integer>> result, ArrayList<Integer> subList) {
         if (target <= 0) {
             return;
         }
@@ -31,14 +31,39 @@ public class Prob0039Solution {
     }
 }
 
+class Prob0039Solution2 {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (candidates.length == 0) return result;
+
+        combinationSum(candidates, target, 0, result, new ArrayList<>());
+
+        return result;
+    }
+
+    public void combinationSum(int[] candidates, int target, int start, List<List<Integer>> result, ArrayList<Integer> subList) {
+        if (target <= 0) {
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            subList.add(candidates[i]);
+            if (target == candidates[i]){
+                result.add(new ArrayList<>(subList));
+            }
+            combinationSum(candidates, target - candidates[i], i, result, subList);
+            subList.remove(subList.size() - 1);
+        }
+    }
+}
+
 class Test{
     public static void main(String[] args) {
         int[] nums = {2,3,5};
-        Prob0039Solution solve = new Prob0039Solution();
+        Prob0039Solution2 solve = new Prob0039Solution2();
         List<List<Integer>> lists = solve.combinationSum(nums, 8);
         for (List<Integer> list : lists) {
             System.out.println(list.toString());
-
         }
     }
 }
