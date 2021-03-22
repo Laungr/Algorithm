c47b24c7779316c26867da2da5a12cb12161f89a
Aaron <laugalung@gmail.com>
2021-03-22 22:00:27 +0800

    Prob0092Solution 反转列表的第二种解法

--- src/leetcode/Prob0092ReverseLinkedListII/Prob0092Solution.java
+++ src/leetcode/Prob0092ReverseLinkedListII/Prob0092Solution.java
@@ -1,15 +1,13 @@
 package leetcode.Prob0092ReverseLinkedListII;
 
 import java.util.ArrayDeque;
-import java.util.List;
 import java.util.Queue;
 import java.util.Stack;
 
-public class Prob0092Solution
-{
+public class Prob0092Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if (head == null ) {
-            return head;
+        if (head == null) {
+            return null;
         }
 
         Stack<Integer> stack = new Stack<>();
@@ -18,18 +16,16 @@ public class Prob0092Solution
 
         int cursor = 1;
 
-        while (head != null){
-            if (cursor < left ) {
+        while (head != null) {
+            if (cursor < left) {
                 preQ.offer(head.val);
-            }
-            else if (cursor > right) {
+            } else if (cursor > right) {
                 tailQ.offer(head.val);
-            }
-            else {
+            } else {
                 stack.push(head.val);
             }
 
-            cursor ++;
+            cursor++;
             head = head.next;
 
         }
@@ -38,22 +34,72 @@ public class Prob0092Solution
         ListNode node = new ListNode(-1);
         resultNode.next = node;
 
-        while (!preQ.isEmpty()){
+        while (!preQ.isEmpty()) {
             node.next = new ListNode(preQ.poll());
             node = node.next;
         }
-        while (!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             node.next = new ListNode(stack.pop());
             node = node.next;
         }
-        while (!tailQ.isEmpty()){
+        while (!tailQ.isEmpty()) {
             node.next = new ListNode(tailQ.poll());
             node = node.next;
         }
 
-       return resultNode.next.next ;
+        return resultNode.next.next;
 
     }
+
+}
+
+class Prob0092Solution2 {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+
+        ListNode cursorNode = dummyNode;
+
+        int count = 1;
+
+        while (count++ < left) {
+            cursorNode = cursorNode.next;
+        }
+        ListNode prev = cursorNode;
+
+        while (count++ <= right + 1){
+            cursorNode = cursorNode.next;
+        }
+        ListNode rightNode = cursorNode;
+
+        ListNode leftNode = prev.next;
+        ListNode succ = rightNode.next;
+
+        rightNode.next = null;
+
+        reverseList(leftNode);
+
+        prev.next = leftNode;
+        rightNode.next = succ;
+
+        return dummyNode.next;
+    }
+
+        public ListNode reverseList(ListNode head){
+
+        ListNode prev = null;
+        ListNode succ = head;
+
+        while (succ != null){
+            ListNode tempNode = succ.next;
+            succ.next = prev;
+            prev = succ;
+            succ = tempNode;
+        }
+
+        return prev;
+    }
+
 
 }
 
@@ -61,7 +107,16 @@ public class Prob0092Solution
 class ListNode {
     int val;
     ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
