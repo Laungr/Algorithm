ca445d9853d967323e565069a97754b630dac45d
Aaron <laugalung@gmail.com>
2021-03-27 13:52:21 +0800

    Prob0700Solution 搜索二叉树中的某个节点

---
+++ src/leetcode/Prob0700SearchinaBinarySearchTree/Prob0700Solution.java
@@ -0,0 +1,39 @@
+package leetcode.Prob0700SearchinaBinarySearchTree;
+
+public class Prob0700Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        }
+
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
