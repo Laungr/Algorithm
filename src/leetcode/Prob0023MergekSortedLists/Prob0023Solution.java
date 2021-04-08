57a595cec52a1ef7245ad144617320809474ee0e
Aaron <laugalung@gmail.com>
2021-04-08 21:29:24 +0800

    Prob0023Solution 合并 k 个链表

--- src/leetcode/Prob0023MergekSortedLists/Prob0023Solution.java
+++ src/leetcode/Prob0023MergekSortedLists/Prob0023Solution.java
@@ -2,36 +2,70 @@ package leetcode.Prob0023MergekSortedLists;
 
 public class Prob0023Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode resultNode = null;
-        ListNode curserNode = null;
-
-        //如果链表集合为空的话，则返回null;
-        if (lists.length == 0) return null;
-
-        //遍历链表的集合
-        for (int i = 0; i < lists.length; i++) {
-            int min = 0;
-            //如果对应链表为空，继续循环
-            if (lists[i] == null) continue;
-            //如果对应链表不为空，则比较
-            else if (lists[i].val < min){
-                min = lists[i].val;
-
-
-            }
-
-
-        }
-
-
-        return null;
+        return merge(lists, 0, lists.length - 1);
     }
 
+    /**
+     *
+     * @param lists 输入的链表数组，包含了多个链表
+     * @param l 左边界
+     * @param r 右边界
+     * @return 返回合并后的链表头
+     */
+
+    private ListNode merge(ListNode[] lists, int l, int r) {
+        if (l == r) {
+            return lists[l];
+        }
+        if (l > r) {
+            return null;
+        }
+        int mid = (l + r) >> 1;
+        return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+    }
+
+    /**
+     * 合并两个链表
+     *
+     * @param p 链表 p
+     * @param q 链表 q
+     * @return 合并之后的链表
+     */
+    private ListNode mergeTwoLists(ListNode p, ListNode q) {
+        if (p == null || q == null) {
+            return p == null ? q : p;
+        }
+        ListNode dummyNode = new ListNode(0);
+        ListNode cursorNode = dummyNode;
+        ListNode pNode = p, qNode = q;
+        while (pNode != null && qNode != null) {
+            if (pNode.val > qNode.val) {
+                cursorNode = cursorNode.next = qNode;
+                qNode = qNode.next;
+            } else {
+                cursorNode = cursorNode.next = pNode;
+                pNode = pNode.next;
+            }
+        }
+        cursorNode.next = pNode == null ? qNode : pNode;
+        return dummyNode.next;
+    }
 }
+
+
 class ListNode {
     int val;
     ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
