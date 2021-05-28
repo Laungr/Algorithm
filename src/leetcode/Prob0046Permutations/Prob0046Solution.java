48f5df164e6b3c5f3c2619ffcfa7d962cef7734f
Aaron <laugalung@gmail.com>
2021-05-28 16:00:18 +0800

    Prob0046Solution 全排列问题的回溯解法，采用DFS + 剪枝

--- src/leetcode/Prob0046Permutations/Prob0046Solution.java
+++ src/leetcode/Prob0046Permutations/Prob0046Solution.java
@@ -1,41 +1,57 @@
 package leetcode.Prob0046Permutations;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Prob0046Solution {
     public List<List<Integer>> permute(int[] nums) {
+        // result 是返回的列表
         List<List<Integer>> result = new ArrayList<>();
-        if (nums.length == 0) return result;
-
+        // 数组为空直接返回空列表
+        if (nums.length == 0) {
+            return result;
+        }
+        // 如果数组不是空数组，就进行组合
         combinationSum(nums, result, new ArrayList<>());
-
+        // 最后返回 result
         return result;
     }
 
+    /**
+     * 回溯算法
+     * 本身是一个递归应用
+     * @param nums 输入的数组
+     * @param result 最终返回的结果
+     * @param subList 正是一个从头用到尾的列表，给它增加元素和删除元素的
+     */
     public void combinationSum(int[] nums, List<List<Integer>> result, ArrayList<Integer> subList) {
-        if (subList.size() >= nums.length){
+        // 递归出口，如果当前的 subList 长度正确，则是一个合适的结果，将其添加到 result
+        if (subList.size() == nums.length) {
+            // 需要对 subList 做一个深拷贝
+            // 变量 subList 所指向的列表 在深度优先遍历的过程中只有一份 ，深度优先遍历完成以后，回到了根结点，成为空列表.
+            //在 Java 中，参数传递是 值传递，对象类型变量在传参的过程中，复制的是变量的地址。这些地址被添加到 result 变量，但实际上指向的是同一块内存地址，因此我们会看到 66 个空的列表对象。解决的方法很简单，在 res.add(subList); 这里做一次拷贝即可。
+            result.add(new ArrayList(subList));
+//            result.add(subList);
             return;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (!subList.contains(nums[i]))
-                subList.add(nums[i]);
-            //System.out.println("subList添加了 "+candidates[i]+"    现在是 "+subList.toString());
-            if (subList.size() == nums.length){
-                result.add(new ArrayList(subList));
-                //System.out.println("有一个满足的是: "+ subList.toString());
+        // 
+        for (int num : nums) {
+            if (subList.contains(num)) {
+                continue;
             }
+            subList.add(num);
             combinationSum(nums, result, subList);
-            //System.out.print("剪枝了： "+ subList.get(subList.size() - 1));
+            // 剪枝
             subList.remove(subList.size() - 1);
-            //System.out.println( "  剪枝后 "+ subList.toString() );
         }
     }
 }
 
-class Test{
+class Test {
     public static void main(String[] args) {
-        int[] nums = {2,3,5};
+        int[] nums = {2, 3, 5};
         Prob0046Solution solve = new Prob0046Solution();
         List<List<Integer>> lists = solve.permute(nums);
         for (List<Integer> list : lists) {
