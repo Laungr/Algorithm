8063bca85fbc49729bab97b2e65215e4720b6b18
Aaron <laugalung@gmail.com>
2021-05-28 17:12:38 +0800

    Prob0046Solution 全排列问题的另一种解法，没用DFS，思路类似

--- src/leetcode/Prob0046Permutations/Prob0046Solution.java
+++ src/leetcode/Prob0046Permutations/Prob0046Solution.java
@@ -3,7 +3,11 @@ package leetcode.Prob0046Permutations;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * 全排列问题的回溯算法解法
+ */
 public class Prob0046Solution {
     public List<List<Integer>> permute(int[] nums) {
         // result 是返回的列表
@@ -21,8 +25,9 @@ public class Prob0046Solution {
     /**
      * 回溯算法
      * 本身是一个递归应用
-     * @param nums 输入的数组
-     * @param result 最终返回的结果
+     *
+     * @param nums    输入的数组
+     * @param result  最终返回的结果
      * @param subList 正是一个从头用到尾的列表，给它增加元素和删除元素的
      */
     public void combinationSum(int[] nums, List<List<Integer>> result, ArrayList<Integer> subList) {
@@ -49,11 +54,57 @@ public class Prob0046Solution {
     }
 }
 
+class Prob0046Solution2 {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length == 0) {
+            return result;
+        }
+        permute(nums, 0, nums.length - 1, result);
+        return result;
+    }
+
+    /**
+     *
+     * @param nums 输入的数组
+     * @param cursor 递归遍历到的位置
+     * @param k 冗余变量，nums.length - 1，并不需要
+     * @param result 返回结果
+     */
+    public void permute(int[] nums, int cursor, int k, List<List<Integer>> result) {
+        if (cursor == k) {
+            result.add(Arrays.stream(nums)
+                    .boxed()
+                    .collect(Collectors.toList()));
+        }
+
+        for (int i = cursor; i <= k; i++) {
+            swap(nums, cursor, i);
+            permute(nums, cursor + 1, k, result);
+            swap(nums, cursor, i);
+        }
+    }
+
+    /**
+     * 交换数组中的两个位置的元素
+     *
+     * @param array 输入数组
+     * @param p     位置 p
+     * @param q     位置 q
+     */
+    public void swap(int[] array, int p, int q) {
+        int temp = array[p];
+        array[p] = array[q];
+        array[q] = temp;
+    }
+}
+
 class Test {
     public static void main(String[] args) {
         int[] nums = {2, 3, 5};
         Prob0046Solution solve = new Prob0046Solution();
-        List<List<Integer>> lists = solve.permute(nums);
+        Prob0046Solution2 solve2 = new Prob0046Solution2();
+        List<List<Integer>> lists = solve2.permute(nums);
         for (List<Integer> list : lists) {
             System.out.println(list.toString());
         }
