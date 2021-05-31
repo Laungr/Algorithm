3fdd7c543e9e58702815bc6770a5cea11b203363
Aaron <laugalung@gmail.com>
2021-05-31 17:04:35 +0800

    Prob0055Solution 跳跃游戏，动态规划

---
+++ src/leetcode/Prob0055JumpGame/Prob0055Solution.java
@@ -0,0 +1,38 @@
+package leetcode.Prob0055JumpGame;
+
+import java.util.Arrays;
+
+/**
+ * 跳跃游戏
+ * 给定一个非负整数数组 nums，你最初位于数组的第一个下标。数组中的每个元素代表你在该位置可以跳跃的最大长度。判断你是否能够到达最后一个下标。
+ * 动态规划 时间复杂度 O(N*N)
+ * @author Okaeri
+ */
+public class Prob0055Solution {
+    public boolean canJump(int[] nums) {
+        int len = nums.length;
+        boolean[] dp = new boolean[len];
+        // 初始跳在第一个，因此为 true
+        dp[0] = true;
+        // 后面的进入循环
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] = (i - j <= nums[j]) && dp[j];
+                // 能跳到即退出循环
+                if (dp[i]) {
+                    break;
+                }
+            }
+        }
+        return dp[len - 1];
+    }
+}
+
+class Test{
+    public static void main(String[] args) {
+        Prob0055Solution sol = new Prob0055Solution();
+        int[] nums = {3, 2, 1, 0, 4};
+        boolean canJump = sol.canJump(nums);
+        System.out.println(canJump);
+    }
+}
